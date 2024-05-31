0181ebf3f57242cff2b47161d213e49096708ff2
doin-N <tlatms777@gmail.com>
2024-05-31 18:41:38 +0900

    수정

--- src/actions/post.js
+++ src/actions/post.js
@@ -1,14 +1,14 @@
 import axios from "axios";
-import { useDispatch } from "react-redux";
 
 
 
-export const fetchPosts = async () => {
-    return async function fetchPostsThunk(dispatch, getState) {
+
+export const fetchPosts = () => async function fetchPostsThun(dispatch, getState){
+
 
         const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
         console.log(response)
         dispatch({ type: 'FETCH_POSTS', payload: response.data });
       }
-    }
+    
 
